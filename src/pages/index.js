1b6a4d1fd7dd4370880df8a9e258bb4f4fe54458
astronpvp <98805055+astronpvp@users.noreply.github.com>
2024-09-23 22:21:14 +0200

    my changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,7 +22,7 @@ export default function Home() {
           content="Nivex - Tu fuente de información sobre tecnología y móviles, con comparativas y análisis detallados."
         />
       </Head>
-      
+
       <Header />
       <main className="mt-16">
         <div
@@ -30,7 +30,7 @@ export default function Home() {
         >
           LO MÁS VISTO
         </div>
-        <div className="mt-16 px-5 sm:mx-auto md:items-center md:max-w-7xl">
+        <div className="mt-16 px-5 sm:mx-auto md:items-center md:max-w-7xl flex">
           <ResumenCategoria
             titulo="Los mejores móviles que puedes comprar en 2024"
             linkInterno="/categorias/moviles"
@@ -38,10 +38,7 @@ export default function Home() {
             categoria="MÓVILES"
             descripcion="Ya sea un móvil de última generación o una opción más asequible. Te mostramos lo mejor de Apple, Samsung, Google y más. "
             imagen="https://i.blogs.es/7418e8/mi10tpro/1366_2000.webp"
-          />     
-        
-        </div>    
-        <div className="mt-16 px-5 sm:mx-auto md:items-center md:max-w-7xl">
+          />
           <ResumenCategoria
             titulo="Las mejores televisiones que puedes comprar en 2024"
             linkInterno="/categorias/televisiones"
@@ -50,9 +47,6 @@ export default function Home() {
             descripcion="Desde pantallas OLED hasta televisores 8K, descubre las mejores opciones para disfrutar de tus series y películas favoritas con la mayor calidad de imagen. Samsung, LG, Sony y otros gigantes de la tecnología te traen lo último."
             imagen="https://images.samsung.com/is/image/samsung/latin_UN55KU6500FXZA_001_Front_black?$624_624_PNG$"
           />
-                   
-        </div>    
-        <div className="mt-16 px-5 sm:mx-auto md:items-center md:max-w-7xl">
           <ResumenCategoria
             titulo="Las mejores tablets que puedes comprar en 2024"
             linkInterno="/categorias/tablets"
@@ -61,8 +55,8 @@ export default function Home() {
             descripcion="Encuentra la tablet ideal para trabajar o entretenerte. Ya sea una iPad Pro de Apple o una opción potente de Samsung, aquí te mostramos las mejores opciones para cada necesidad."
             imagen="https://cdn.andro4all.com/andro4all/2024/06/huawei-matepad-11-5-s.png"
           />
-          </div>
-          <div className="mt-16 px-5 sm:mx-auto md:items-center md:max-w-7xl">
+
+
           <ResumenCategoria
             titulo="Los mejores auriculares/in-ear que puedes comprar en 2024"
             linkInterno="/categorias/auriculares"
@@ -71,8 +65,7 @@ export default function Home() {
             descripcion="Sumérgete en la música con los auriculares más innovadores del momento. Desde los AirPods de Apple hasta las opciones de Sony y Bose, aquí están los mejores modelos con cancelación de ruido y sonido premium."
             imagen="https://cdn.avpasion.com/wp-content/uploads/2024/09/mejores-auriculares-204-14.webp"
           />
-          </div>
-          <div className="mt-16 px-5 sm:mx-auto md:items-center md:max-w-7xl">
+
           <ResumenCategoria
             titulo="Los mejores portátiles que puedes comprar en 2024"
             linkInterno="/categorias/portatiles"
@@ -81,8 +74,7 @@ export default function Home() {
             descripcion="Ya sea para gaming, productividad o edición de contenido, descubre los portátiles que lideran el mercado. Te mostramos lo último de Dell, HP, Apple, y otros para ayudarte a elegir."
             imagen="https://leasein.co/wp-content/uploads/2024/03/notebook-xps-15-9530-t-black-gallery-5.webp"
           />
-          </div>
-          <div className="mt-16 px-5 sm:mx-auto md:items-center md:max-w-7xl">
+
           <ResumenCategoria
             titulo="Los mejores wearables que puedes comprar en 2024"
             linkInterno="/categorias/wearables"
@@ -91,11 +83,20 @@ export default function Home() {
             descripcion="Mantente conectado con los wearables más avanzados del mercado. Desde los Apple Watch hasta los Galaxy Watch, descubre los dispositivos que mejoran tu salud, fitness y estilo."
             imagen="https://www.wivai.com/on/demandware.static/-/Sites-promocaixa-m-catalog/default/dw4b369b19/deporteyocio/Garmin/121-4004953/GarminEPIX2_FrontA_600x600.png"
           />
-          </div>
-          
-        
+          <button id="arrowLeft" className="w-7 h-7 absolute">
+            <img className="" src="https://cdn-icons-png.flaticon.com/512/271/271220.png" />
+          </button>
+          <button id="arrowRight" className="w-7 h-7 absolute left-">
+            <img className="" src="https://cdn-icons-png.flaticon.com/512/32/32213.png" />
+          </button>
+
+        </div>
+
+
+
+
       </main>
-      
+
     </>
   );
 }
