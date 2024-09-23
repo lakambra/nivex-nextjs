aa4e6d4d2ea03cdefdc276d9700efb0d46dd3beb
astronpvp <98805055+astronpvp@users.noreply.github.com>
2024-09-23 22:21:14 +0200

    my changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -48,38 +48,37 @@ export default function Header() {
             TELEVISIONES
           </Link>
           <Link
-  href="/categorias/tablets"
-  className={`${fontJet.className} ml-8 text-sm font-bold inline-block relative text-black no-underline transition-all duration-[0.3s] ease-in-out after:content-normal after:absolute after:w-full after:h-0.5 after:bg-black after:origin-bottom-right after:transition-transform after:duration-[0.25s] after:ease-[ease-out] after:scale-x-0 after:left-0 after:bottom-0 hover:after:origin-bottom-left hover:after:scale-x-100`}
->
-  TABLETS
-</Link>
+            href="/categorias/tablets"
+            className={`${fontJet.className} ml-8 text-sm font-bold inline-block relative text-black no-underline transition-all duration-[0.3s] ease-in-out after:content-normal after:absolute after:w-full after:h-0.5 after:bg-black after:origin-bottom-right after:transition-transform after:duration-[0.25s] after:ease-[ease-out] after:scale-x-0 after:left-0 after:bottom-0 hover:after:origin-bottom-left hover:after:scale-x-100`}
+          >
+            TABLETS
+          </Link>
 
-<Link
-  href="/categorias/auriculares"
-  className={`${fontJet.className} ml-8 text-sm font-bold inline-block relative text-black no-underline transition-all duration-[0.3s] ease-in-out after:content-normal after:absolute after:w-full after:h-0.5 after:bg-black after:origin-bottom-right after:transition-transform after:duration-[0.25s] after:ease-[ease-out] after:scale-x-0 after:left-0 after:bottom-0 hover:after:origin-bottom-left hover:after:scale-x-100`}
->
-  AURICULARES
-</Link>
+          <Link
+            href="/categorias/auriculares"
+            className={`${fontJet.className} ml-8 text-sm font-bold inline-block relative text-black no-underline transition-all duration-[0.3s] ease-in-out after:content-normal after:absolute after:w-full after:h-0.5 after:bg-black after:origin-bottom-right after:transition-transform after:duration-[0.25s] after:ease-[ease-out] after:scale-x-0 after:left-0 after:bottom-0 hover:after:origin-bottom-left hover:after:scale-x-100`}
+          >
+            AURICULARES
+          </Link>
 
-<Link
-  href="/categorias/portatiles"
-  className={`${fontJet.className} ml-8 text-sm font-bold inline-block relative text-black no-underline transition-all duration-[0.3s] ease-in-out after:content-normal after:absolute after:w-full after:h-0.5 after:bg-black after:origin-bottom-right after:transition-transform after:duration-[0.25s] after:ease-[ease-out] after:scale-x-0 after:left-0 after:bottom-0 hover:after:origin-bottom-left hover:after:scale-x-100`}
->
-  PORTÁTILES
-</Link>
+          <Link
+            href="/categorias/portatiles"
+            className={`${fontJet.className} ml-8 text-sm font-bold inline-block relative text-black no-underline transition-all duration-[0.3s] ease-in-out after:content-normal after:absolute after:w-full after:h-0.5 after:bg-black after:origin-bottom-right after:transition-transform after:duration-[0.25s] after:ease-[ease-out] after:scale-x-0 after:left-0 after:bottom-0 hover:after:origin-bottom-left hover:after:scale-x-100`}
+          >
+            PORTÁTILES
+          </Link>
 
-<Link
-  href="/categorias/wearables"
-  className={`${fontJet.className} ml-8 text-sm font-bold inline-block relative text-black no-underline transition-all duration-[0.3s] ease-in-out after:content-normal after:absolute after:w-full after:h-0.5 after:bg-black after:origin-bottom-right after:transition-transform after:duration-[0.25s] after:ease-[ease-out] after:scale-x-0 after:left-0 after:bottom-0 hover:after:origin-bottom-left hover:after:scale-x-100`}
->
-  WEARABLES
-</Link>
+          <Link
+            href="/categorias/wearables"
+            className={`${fontJet.className} ml-8 text-sm font-bold inline-block relative text-black no-underline transition-all duration-[0.3s] ease-in-out after:content-normal after:absolute after:w-full after:h-0.5 after:bg-black after:origin-bottom-right after:transition-transform after:duration-[0.25s] after:ease-[ease-out] after:scale-x-0 after:left-0 after:bottom-0 hover:after:origin-bottom-left hover:after:scale-x-100`}
+          >
+            WEARABLES
+          </Link>
         </nav>
       </div>
       <nav
-        className={`flex flex-col gap-6 transition-all duration-500 ease-out fixed ${
-          isMenuOpen ? "h-screen bg-white w-full px-5 py-10 z-10" : "h-0"
-        }`}
+        className={`flex flex-col gap-6 transition-all duration-500 ease-out fixed ${isMenuOpen ? "h-screen bg-white w-full px-5 py-10 z-10" : "h-0"
+          }`}
       >
         {isMenuOpen && (
           <>
@@ -88,6 +87,39 @@ export default function Header() {
               className={`${fontJet.className} text-2xl inline-block relative text-black no-underline transition-all duration-[0.3s] ease-in-out after:content-normal after:absolute after:w-full after:h-0.5 after:bg-black after:origin-bottom-right after:transition-transform after:duration-[0.25s] after:ease-[ease-out] after:scale-x-0 after:left-0 after:bottom-0 hover:after:origin-bottom-left hover:after:scale-x-100`}
             >
               MÓVILES
+            </Link>
+            <Link
+              href="/categorias/televisiones"
+              className={`${fontJet.className} text-2xl inline-block relative text-black no-underline transition-all duration-[0.3s] ease-in-out after:content-normal after:absolute after:w-full after:h-0.5 after:bg-black after:origin-bottom-right after:transition-transform after:duration-[0.25s] after:ease-[ease-out] after:scale-x-0 after:left-0 after:bottom-0 hover:after:origin-bottom-left hover:after:scale-x-100`}
+            >
+              TELEVISIONES
+            </Link>
+            <Link
+              href="/categorias/tablets"
+              className={`${fontJet.className} text-2xl inline-block relative text-black no-underline transition-all duration-[0.3s] ease-in-out after:content-normal after:absolute after:w-full after:h-0.5 after:bg-black after:origin-bottom-right after:transition-transform after:duration-[0.25s] after:ease-[ease-out] after:scale-x-0 after:left-0 after:bottom-0 hover:after:origin-bottom-left hover:after:scale-x-100`}
+            >
+              TABLETS
+            </Link>
+
+            <Link
+              href="/categorias/auriculares"
+              className={`${fontJet.className} text-2xl inline-block relative text-black no-underline transition-all duration-[0.3s] ease-in-out after:content-normal after:absolute after:w-full after:h-0.5 after:bg-black after:origin-bottom-right after:transition-transform after:duration-[0.25s] after:ease-[ease-out] after:scale-x-0 after:left-0 after:bottom-0 hover:after:origin-bottom-left hover:after:scale-x-100`}
+            >
+              AURICULARES
+            </Link>
+
+            <Link
+              href="/categorias/portatiles"
+              className={`${fontJet.className} text-2xl inline-block relative text-black no-underline transition-all duration-[0.3s] ease-in-out after:content-normal after:absolute after:w-full after:h-0.5 after:bg-black after:origin-bottom-right after:transition-transform after:duration-[0.25s] after:ease-[ease-out] after:scale-x-0 after:left-0 after:bottom-0 hover:after:origin-bottom-left hover:after:scale-x-100`}
+            >
+              PORTÁTILES
+            </Link>
+
+            <Link
+              href="/categorias/wearables"
+              className={`${fontJet.className} text-2xl inline-block relative text-black no-underline transition-all duration-[0.3s] ease-in-out after:content-normal after:absolute after:w-full after:h-0.5 after:bg-black after:origin-bottom-right after:transition-transform after:duration-[0.25s] after:ease-[ease-out] after:scale-x-0 after:left-0 after:bottom-0 hover:after:origin-bottom-left hover:after:scale-x-100`}
+            >
+              WEARABLES
             </Link>
             <div className="border-t-[1px] border-t-black pt-4">
               SOBRE NIVEX
