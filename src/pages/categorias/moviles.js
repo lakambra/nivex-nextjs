7c8e137207dec39289de18476da6d2e4a942cb25
astronpvp <98805055+astronpvp@users.noreply.github.com>
2024-09-24 11:11:16 +0200

    resumen categoria grande y algun que otro cambio

--- src/pages/categorias/moviles.js
+++ src/pages/categorias/moviles.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 import Head from "next/head";
 import Header from "@/components/Header";
 import Breadcrumbs from "@/components/Breadcrumbs";
-import ResumenCategoria from "@/components/ResumenCategoria";
+import ResumenCategoriaGrande from "@/components/ResumenCategoriaGrande";
 import { Lato } from "next/font/google";
 import { Oswald } from "next/font/google";
 
@@ -32,13 +32,22 @@ export default function Moviles() {
           </h2>
         </div>
         <div className="overflow-x-hidden px-5 mb-5 md:max-w-6xl mx-auto">
-          <ResumenCategoria
+          <ResumenCategoriaGrande
             titulo="Los mejores móviles que puedes comprar en 2024"
             linkInterno="/categorias/moviles"
             linkExterno="/categorias/moviles/mejor-movil-de-2024-test"
             categoria="MÓVILES"
             descripcion="Ya sea un móvil de última generación o una opción más asequible. Te mostramos lo mejor de Apple, Samsung, Google y más. "
             imagen="https://i.blogs.es/7418e8/mi10tpro/1366_2000.webp"
+          />
+
+          <ResumenCategoriaGrande
+            titulo="Los nuevos plegables que revolucionan el mercado en 2024"
+            linkInterno="/categorias/moviles"
+            linkExterno="/categorias/moviles/novedades-2024"
+            categoria="MÓVILES"
+            descripcion="Descubre los últimos modelos de móviles plegables que combinan innovación y diseño. Desde el Galaxy Z Fold 4 hasta el nuevo Motorola Razr, te mostramos los dispositivos más innovadores del año."
+            imagen="https://unaysahbd.com/wp-content/uploads/2024/08/Post_Template_12.webp"
           />
         </div>
       </main>
