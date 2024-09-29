9de4641e30f8b44a6c61b145fafedeaf35d528c8
astronpvp <98805055+astronpvp@users.noreply.github.com>
2024-09-29 12:16:17 +0200

    cambios en dbamazon y nuevos componentes

---
+++ src/components/ProductoAmazon.js
@@ -0,0 +1,44 @@
+import { JetBrains_Mono } from "next/font/google";
+import { Lato } from "next/font/google";
+import { Oswald } from "next/font/google";
+
+const fontJet = JetBrains_Mono({ subsets: ["latin"] });
+
+const fontLeto = Lato({
+    subsets: ["latin"],
+    weight: ["100", "300", "400", "700", "900"],
+});
+const fontOswald = Oswald({ subsets: ["latin"] });
+
+export default function ProductoAmazon({
+    titulo,
+    linkAmazon,
+    precio,
+    descripcion,
+    imagen,
+}) {
+    return (
+        <>
+            <div className="flex flex-col flex flex-column justify-center items-center w-72 ">
+                <img
+                    src={imagen}
+                    className="w-40 h-48 object-cover object-center max-w-5xl mb-5"
+                />
+                <div className="flex flex-col justify-center ">
+                    <h2 className="text-xl font-extrabold tracking-wide hover:underline ">
+                        {titulo}
+                    </h2>
+                    <hr className="w-20 border-b-[1px] border-b-blue-700 mt-2 mb-5 lg:mt-4"></hr>
+                    <h3 className="text-lg text- mb-5">
+                        {descripcion}
+                    </h3>
+                    <a href={linkAmazon} target="_blank">
+                        <button className="text-sm bg-[#FFD814] rounded-lg p-1 px-2 transition-all hover:bg-[#F7CA00] mb-10">
+                            Precio en Amazon - <strong>{precio}</strong>
+                        </button>
+                    </a>
+                </div>
+            </div>
+        </>
+    );
+}
