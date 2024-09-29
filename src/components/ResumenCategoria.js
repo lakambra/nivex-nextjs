21d7bba7013dbc2e6bff3bde2bbcc251268185f1
astronpvp <98805055+astronpvp@users.noreply.github.com>
2024-09-29 13:03:38 +0200

    televisiones

--- src/components/ResumenCategoria.js
+++ src/components/ResumenCategoria.js
@@ -22,13 +22,13 @@ export default function ResumenCategoria({
   return (
     <>
       <div className="flex flex-col justify-center flex flex-column justify-center items-center w-80 mb-5">
-        <Link href={linkExterno}>
+        <Link href={linkExterno} className="w-full">
           <img
             src={imagen}
-            className="w-40 h-40 object-cover object-center max-w-5xl "
+            className="w-3/4 h-60 object-cover object-center m-auto"
           />
         </Link>
-        <div className="flex flex-col justify-center items-center">
+        <div className="flex flex-col justify-center">
           <Link href={linkInterno} className={fontJet.className}>
             <p className="text-xs font-bold pt-5 mb-2 inline-block relative text-black no-underline transition-all duration-[0.3s] ease-in-out after:content-normal after:absolute after:w-full after:h-0.5 after:bg-black after:origin-bottom-right after:transition-transform after:duration-[0.25s] after:ease-[ease-out] after:scale-x-0 after:left-0 after:bottom-0 hover:after:origin-bottom-left hover:after:scale-x-100">
               {categoria}
@@ -39,7 +39,7 @@ export default function ResumenCategoria({
               {titulo}
             </h2>
           </Link>
-          <hr className="w-20 border-b-[1px] border-b-blue-700 mt-2 mb-5 lg:mt-4"></hr>
+          <hr className="w-20 border-b-[1px] border-b-blue-700 mt-2 mb-5 lg:mt-4 "></hr>
           <Link href={linkExterno}>
             <h3 className="text-lg text-justify">
               {descripcion}
--- src/pages/categorias/televisiones/mejoresTV2024.js
+++ src/pages/categorias/televisiones/mejoresTV2024.js
@@ -4,6 +4,7 @@ import MovilDetallado from "@/components/MovilDetallado";
 import { JetBrains_Mono } from "next/font/google";
 import { Oswald } from "next/font/google";
 import { Lato } from "next/font/google";
+import dbTV from "@/data/DBAmazon";
 
 const fontLato = Lato({
   subsets: ["latin"],
@@ -30,7 +31,7 @@ export default function mejoresTV2024() {
           <h1
             className={`${fontOswald.className} text-3xl font-bold tracking-wide xl:text-5xl`}
           >
-            Las mejores televisiones de 2024 en relación calidad-precio
+            Las mejores televisiones que puedes comprar en 2024
           </h1>
           <p className="font-bold mt-6 lg:text-lg">
             Hay televisores en todos los precios imaginables: desde menos
@@ -46,17 +47,20 @@ export default function mejoresTV2024() {
           className="mt-10 px-5 xl:max-w-5xl xl:mx-auto"
         />
         <p className="px-5 mt-10 font-bold xl:max-w-5xl xl:mx-auto xl:text-lg mb-5">
-          Un año más, buscamos los televisores con la mejor relación calidad-precio de este 2024, enfocándonos en aquellos modelos que maximizan el rendimiento dentro de su rango de precio. Incluiremos en nuestra lista los televisores disponibles este año, aunque hayan sido lanzados con anterioridad.
+          Un año más, buscamos los mejores televisores de 2024, centrándonos en aquellos modelos que destacan por su innovación, calidad de imagen y rendimiento. En nuestra selección incluimos los televisores más avanzados del mercado, equipados con tecnologías de última generación como paneles OLED, Mini LED, resoluciones 8K y funciones de inteligencia artificial. Estos modelos ofrecen la mejor experiencia de visualización para cine en casa, gaming y entretenimiento, asegurando un nivel de calidad superior para los usuarios más exigentes.
         </p>
         <div className="px-5 xl:max-w-5xl xl:mx-auto">
-          <MovilDetallado
-            producto="Samsung CU6905"
-            imagen="https://i.blogs.es/edfab2/captura-de-pantalla-2024-08-07-a-las-9.33.12/1366_2000.png"
-            descripcion="El Samsung CU6905 es el televisor más asequible de Samsung en 2024, pero eso no significa que carezca de calidad. Ofrece un diseño moderno y elegante, y su rendimiento de imagen destaca por la claridad y el brillo. Además, cuenta con la interfaz Smart TV de Samsung, lo que garantiza acceso fácil a aplicaciones y contenido, así como actualizaciones frecuentes para mantener tu experiencia al día."
-            productoAmazon="Samsung CU6905 - SAMSUNG TV Crystal UHD 4K 2024 43CU7095 Smart TV de 43 con PurColor, Procesador Crystal UHD, SmartThings, Contrast Enhancer con HDR10+ y Smart TV Powered by Tizen [Clase de eficiencia energética G]"
-            precio="348,92€"
-            linkProductoAmazon="https://www.amazon.es/SAMSUNG-43CU7095-PurColor-Procesador-SmartThings/dp/B0CSDTTJ9W/ref=sr_1_1?__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&dib=eyJ2IjoiMSJ9.YNkHwkyIAABzSYGqSKHeGnJ5WyGbOBQdMTZZK_PZV4GWPSN6yM5jB7Z_Y914lV0i.idE33-3pbFx8jKsfQCc2e0kZcDiPZcsBUUgwSMUte_E&dib_tag=se&keywords=Samsung+CU6905&qid=1726953551&s=electronics&sr=1-1&ufe=INHOUSE_INSTALLMENTS%3AES_IHI_4M_AUTOMATED"
-          />
+          {dbTV.televisiones.topGama.map((categoria, index) => (
+            <MovilDetallado
+              key={index}
+              producto={categoria.producto}
+              imagen={categoria.imagen}
+              descripcion={categoria.descripcion}
+              productoAmazon={categoria.productoAmazon}
+              precio={categoria.precio}
+              linkProductoAmazon={categoria.linkProductoAmazon}
+            />
+          ))}
         </div>
       </main>
     </>
