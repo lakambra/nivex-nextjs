d5ced932ab7531381dedde10cedc48ca70a0d9f0
lakambra <joellacambra02@gmail.com>
2024-09-30 13:41:39 +0200

    subseccion: puesto key

--- src/components/ResumenCategoriaGrande.js
+++ src/components/ResumenCategoriaGrande.js
@@ -28,7 +28,7 @@ export default function ResumenCategoria({
                         className="w-60 h-60 object-cover object-center max-w-5xl "
                     />
                 </Link>
-                <div className="flex flex-col w-2/4">
+                <div className="flex flex-col w-11/12 md:max-w-[40%]">
                     <Link href={linkInterno} className={fontJet.className}>
                         <p className="text-xs font-bold pt-5 mb-2 inline-block relative text-black no-underline transition-all duration-[0.3s] ease-in-out after:content-normal after:absolute after:w-full after:h-0.5 after:bg-black after:origin-bottom-right after:transition-transform after:duration-[0.25s] after:ease-[ease-out] after:scale-x-0 after:left-0 after:bottom-0 hover:after:origin-bottom-left hover:after:scale-x-100">
                             {categoria}
