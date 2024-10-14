cb86e5d01e4a17aa2e8181386ebbf56295966ea8
lakambra <joellacambra02@gmail.com>
2024-10-14 09:16:16 +0200

    added analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import { DM_Sans } from "next/font/google";
 import Footer from "@/components/Footer";
+import { Analytics } from '@vercel/analytics/react';
 
 const font = DM_Sans({ subsets: ["latin"] });
 
@@ -8,8 +9,8 @@ export default function App({ Component, pageProps }) {
   return (
     <div className={font.className}>
       <Component {...pageProps} />
+      <Analytics />
       <Footer></Footer>
-
     </div>
   );
 }
