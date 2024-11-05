11e7d62e9bbd0b49a23e772de6005b8c3360b88f
astronpvp <98805055+astronpvp@users.noreply.github.com>
2024-11-05 20:33:41 +0100

    anuncios de busqueda en google

--- src/components/Heads.js
+++ src/components/Heads.js
@@ -16,14 +16,22 @@ export default function Heads({
             <title>{titulo}</title>
             <link rel="icon" type="image/png" href="/images/1.png" />
             <meta name="robots" content="index,follow" />
-            <meta
-                name="google-site-verification"
-                content="rDJ6SDzDvNgj_Bq75dweJgoe9o-EWl3cmOhG2NecGNE"
-            />
-            <meta
-                name="description"
-                content={descripcion}
+            <meta name="google-site-verification" content="rDJ6SDzDvNgj_Bq75dweJgoe9o-EWl3cmOhG2NecGNE" />
+            <meta name="description" content={descripcion} />
+
+            <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16735302617"></script>
+            <script
+                dangerouslySetInnerHTML={{
+                    __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'AW-16735302617');
+            `,
+                }}
             />
         </Head>
+
     );
 }
